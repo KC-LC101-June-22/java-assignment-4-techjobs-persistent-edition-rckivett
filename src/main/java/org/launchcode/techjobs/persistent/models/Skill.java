6e1034d2baaad691891b97a3cc33a6e11d312874
rckivett <rckivett@gmail.com>
2022-11-16 00:25:12 -0600

    Parts 1, 2, and 3 all tests passing

--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,27 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    @NotBlank
+    @Size(min=2, max=100, message = "Skills must be between 2 and 100 characters.")
+    private String description;
+
+    public Skill (String description){
+        this.description = description;
+    }
+
+    public Skill(){};
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
